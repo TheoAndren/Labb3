d23db7ef16237cb101439cd9731ad7c075d35de6
TheoAndren <91311313+TheoAndren@users.noreply.github.com>
2021-09-30 13:55:05 +0200

    Added "NumbersGame" Folder
    Added all files from the folder "NumbersGame"

---
+++ NumbersGame/NumbersGame/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumbersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan 1-20. Kan du gissa vilket? Du får fem försök.");
+            Random random = new Random();
+            int number = random.Next(1, 20);
+            int guesses = 1;
+            
+            while (guesses <= 5)
+            {
+               int userGuess = Convert.ToInt32(Console.ReadLine());
+                CheckGuess(number, userGuess);
+                guesses++;
+            }
+            Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
+            Environment.Exit(0);
+
+        }
+        static void CheckGuess(int number, int userGuess)
+        {
+            
+            if (userGuess > number)
+            {
+                Console.WriteLine("Tyvärr du gissade för högt");
+            }
+
+            else if (userGuess < number)
+            {
+                Console.WriteLine("Tyvärr du gissade för lågt");
+            }
+
+            else if ((userGuess == number))
+            {
+                Console.WriteLine("Du gissade rätt! Wohooo!");
+                Environment.Exit(0);
+            }
+    
+        }
+    } 
+
+}
